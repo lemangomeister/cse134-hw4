f6bfb6f01637be253a5adcbacf25cb2324242e68
lemangomeister <maguinto1221@gmail.com>
2023-11-28 20:52:26 -0800

    modified validation messages (part 3)

--- part3.js
+++ part3.js
@@ -7,20 +7,39 @@ let comment = document.getElementById('comments');
 // console.log(comment);
 
 
-if(inputName.value === ""){
+inputName.addEventListener("input", () =>{
+  if(inputName.value === ""){
     inputName.setCustomValidity("U got a name or nah ?");
-}
-else if(inputName.checkValidity() === false){
+  }
+  else if(inputName.patternMismatch){
     inputName.setCustomValidity("Only names using the Latin alphabet are accepted");
-}
+  }
+  else{
+    inputName.setCustomValidity("");
+  }
+} )
 
-if(email.checkValidity() === false){
-  email.setCustomValidity("Dat email no gud fam, put a legit one frfr");
-}
+email.addEventListener("input", () =>{
+  if(email.value === ""){
+    email.setCustomValidity("U got an email or nah ?");
+  }
+  else if(email.checkValidity() === false){
+    email.setCustomValidity("Dat email no gud fam, put a legit one frfr");
+  }
+  else{
+    email.setCustomValidity("");
+  }
+})
 
-if(comment.value === ""){
-  comment.setCustomValidity("Share somethin fam");
-}
-else if(comment.checkValidity() === false){
-  comment.setCustomValidity("Some characters are not accepted");
-}
+
+comment.addEventListener("input", () => {
+  if(comment.value.length === 0){
+    comment.setCustomValidity("Share somethin fam");
+  }
+  else if(comment.checkValidity() === false){
+    comment.setCustomValidity("Some characters are not accepted");
+  }
+  else{
+    comment.setCustomValidity("");
+  }
+})
